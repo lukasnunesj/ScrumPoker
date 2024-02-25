3c8d656fde0d2742dab04694c15f27e1a4db4088
Lucas Nunes <lukasnunesj@gmail.com>
2024-02-25 18:17:46 -0300

    setting up jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+  clearMocks: true,
+  collectCoverageFrom: [
+    './src/**/*.{js,ts}',
+    '!**/migrations/**/*.{js,ts}',
+    '!**/*.entity.{js,ts}'
+  ],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+
+
 };
